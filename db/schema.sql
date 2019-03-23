25f56d9a42a087539f1d2fc33ed808ad34fb7e8b
Matteo <mmarshburn@smu.edu>
2019-03-22 19:28:58 -0500

    728

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db; 
+USE burgers_db; 
+CREATE TABLE burgers
+ (
+  id INTEGER AUTO_INCREMENT, 
+  burger_name VARCHAR (150) NOT NULL, 
+  devoured BOOLEAN, 
+  PRIMARY KEY (id)
+); 
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Double Trouble', true);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Hot Bastard', false);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Quadruple Bacon', true);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Fancy Sauce Surprise', false);
+INSERT INTO burgers (burger_name, devoured ) VALUES ('Cheesy Jalapeno', true);
